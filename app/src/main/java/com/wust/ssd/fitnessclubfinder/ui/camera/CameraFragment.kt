91eab41c067fe7f1a413583807a28ba83325aff6
jBosak98 <jbosak98@gmail.com>
2019-12-26 21:26:57 +0100

    changed surfaceView to arFragment

--- app/src/main/java/com/wust/ssd/fitnessclubfinder/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/wust/ssd/fitnessclubfinder/ui/camera/CameraFragment.kt
@@ -1,35 +1,36 @@
 package com.wust.ssd.fitnessclubfinder.ui.camera
 
-import android.opengl.GLSurfaceView
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import com.google.ar.sceneform.ux.ArFragment
 import com.wust.ssd.fitnessclubfinder.R
 import com.wust.ssd.fitnessclubfinder.di.Injectable
 import javax.inject.Inject
 
 class CameraFragment : Fragment(), Injectable {
 
+    private val TAG = "CAMERA_FRAGMENT"
+
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private var viewModel: CameraViewModel? = null
 
-    private lateinit var cameraViewModel: CameraViewModel
+    private lateinit var arFragment: ArFragment
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         if (viewModel == null) {
-            viewModel =
-                ViewModelProviders.of(this, viewModelFactory).get(CameraViewModel::class.java)
+            viewModel = ViewModelProviders
+                .of(this, viewModelFactory)
+                .get(CameraViewModel::class.java)
         }
     }
 
@@ -38,16 +39,19 @@ class CameraFragment : Fragment(), Injectable {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        cameraViewModel =
-//            ViewModelProviders.of(this).get(CameraViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_camera, container, false)
+
         val textView: TextView = root.findViewById(R.id.text_camera)
+        textView.text = "Camera fragment"
 //        cameraViewModel.text.observe(this, Observer {
 //            textView.text = it
 //        })
 
-//        val surfaceView: GLSurfaceView = root.findViewById(R.id.surfaceView)
-
+        arFragment = childFragmentManager.findFragmentById(R.id.ar_fragment) as ArFragment
+        arFragment.arSceneView.scene.addOnUpdateListener { frameTime ->
+            arFragment.onUpdate(frameTime)
+        }
         return root
     }
 
