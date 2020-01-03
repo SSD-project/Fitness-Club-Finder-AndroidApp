8ef28cdbc9177f44ef5fa9d820ba8d2d76484092
jBosak98 <jbosak98@gmail.com>
2020-01-03 22:42:16 +0100

    provide restApiRepository, RxScheduler and nearbySearchRepo

--- app/src/main/java/com/wust/ssd/fitnessclubfinder/di/AppModule.kt
+++ app/src/main/java/com/wust/ssd/fitnessclubfinder/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.wust.ssd.fitnessclubfinder.di
 
+import android.app.Application
 import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -10,9 +11,13 @@ import com.wust.ssd.fitnessclubfinder.App
 import com.wust.ssd.fitnessclubfinder.common.BackgroundRenderer
 import com.wust.ssd.fitnessclubfinder.common.DisplayRotationHelper
 import com.wust.ssd.fitnessclubfinder.common.MainRenderer
+import com.wust.ssd.fitnessclubfinder.common.network.RetrofitFactory
+import com.wust.ssd.fitnessclubfinder.common.repository.NearbySearchRepository
+import com.wust.ssd.fitnessclubfinder.common.repository.RestApiRepository
 import com.wust.ssd.fitnessclubfinder.ui.camera.CameraFragmentComponent
 import com.wust.ssd.fitnessclubfinder.ui.login.LoginActivityComponent
 import com.wust.ssd.fitnessclubfinder.ui.main.MainActivityComponent
+import com.wust.ssd.fitnessclubfinder.utils.RxSchedulersFacade
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -63,4 +68,21 @@ class AppModule {
         backgroundRenderer: BackgroundRenderer,
         displayRotationHelper: DisplayRotationHelper
     ) = MainRenderer(app.applicationContext, displayRotationHelper, backgroundRenderer)
+
+    @Singleton
+    @Provides
+    fun provideRestApiRepository(): RestApiRepository = RestApiRepository(RetrofitFactory())
+
+    @Provides
+    fun provideRxSchedulersFacade(): RxSchedulersFacade = RxSchedulersFacade()
+
+    @Singleton
+    @Provides
+    fun provideNearbySearchRepository(
+        app: Application,
+        restApiRepository: RestApiRepository,
+        rxSchedulersFacade: RxSchedulersFacade
+    ): NearbySearchRepository =
+        NearbySearchRepository(restApiRepository, app.applicationContext, rxSchedulersFacade)
+
 }
