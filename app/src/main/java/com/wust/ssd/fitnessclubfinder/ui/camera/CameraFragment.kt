bb213beeb1c90459ce67506c6a745e4791c4b155
jBosak98 <jbosak98@gmail.com>
2020-01-03 22:43:52 +0100

    CameraFragment as LocationListener

--- app/src/main/java/com/wust/ssd/fitnessclubfinder/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/wust/ssd/fitnessclubfinder/ui/camera/CameraFragment.kt
@@ -1,23 +1,31 @@
 package com.wust.ssd.fitnessclubfinder.ui.camera
 
+import android.annotation.SuppressLint
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.wust.ssd.fitnessclubfinder.R
 import com.wust.ssd.fitnessclubfinder.common.MainRenderer
+import com.wust.ssd.fitnessclubfinder.common.model.UserLocation
 import com.wust.ssd.fitnessclubfinder.di.Injectable
 import javax.inject.Inject
 
-class CameraFragment : Fragment(), Injectable {
+class CameraFragment : Fragment(), Injectable, LocationListener {
 
-    private val TAG = this.javaClass.simpleName
+    private val TAG = "CameraFragment"
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -29,6 +37,7 @@ class CameraFragment : Fragment(), Injectable {
 
     private lateinit var mSurfaceView: GLSurfaceView
 
+    @SuppressLint("MissingPermission")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
@@ -37,7 +46,23 @@ class CameraFragment : Fragment(), Injectable {
                 .of(this, viewModelFactory)
                 .get(CameraViewModel::class.java)
 
+        viewModel!!.requestUserLocationUpdates()
+
+        viewModel?.location?.observe(this, Observer {
+            Log.e("CameraLocation", it.latitude.toString())
+        })
+        viewModel?.nearbyClubs?.observe(this, Observer {
+            Log.e(TAG,it.toString())
+        })
+        viewModel?.nearbySearchRepository?.startNearbyClubsApiCalls()
+
+        val locationManager = activity?.getSystemService(AppCompatActivity.LOCATION_SERVICE) as LocationManager
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this)
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, this)
+
+
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -50,6 +75,7 @@ class CameraFragment : Fragment(), Injectable {
         viewModel?.text?.observe(this, Observer {
             textView.text = it
         })
+
         mSurfaceView = root.findViewById(R.id.surface_view)
 
         mSurfaceView.apply {
@@ -78,4 +104,18 @@ class CameraFragment : Fragment(), Injectable {
         renderer.onResume()
         mSurfaceView.onResume()
     }
+
+    override fun onLocationChanged(userLocation: Location?) {
+        Toast.makeText(context, "$TAG onLocationChanged", Toast.LENGTH_SHORT).show()
+        userLocation?.let {
+            viewModel?.onLocationChanged(it)
+        }
+
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {}
+
+    override fun onProviderEnabled(p0: String?) {}
+
+    override fun onProviderDisabled(p0: String?) {}
 }
